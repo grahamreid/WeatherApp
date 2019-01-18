24cabd2aa724e7c2852a8bf4a195d1c254a36a26
root <root@DESKTOP-D5ODQ3P.localdomain>
2019-01-17 21:31:40 -0500

    add location route and methods to post and get locations

--- src/backend/models/user.js
+++ src/backend/models/user.js
@@ -1,10 +1,38 @@
 export default class User {
     constructor(name) {
         this._name = name
+        this._locations = ['first place']
     }
     
     //TODO: Add unit test
-    get() {
+    //TODO: Throw specific error type for user already exists
+    async get() {
+        // throw new Error(`User ${this._name} does not exist.`)
         return this._name
     }
+
+    async get_locations() {
+        return this._locations
+    }
+
+    //TODO: try/catch
+    async add_location(location) {
+        return this.get_locations()
+                .then(locations => {
+                    locations.push(location)
+                    return locations
+                })
+                .then(locations => {return this.save(locations)})
+    }
+    
+    //TODO: Add unit test
+    //TODO: check if name already exists
+    //TODO: throw exception if name already exists
+    //TODO: get location, append new, and then save to redis
+    async save(locations) {
+        this._locations = locations
+        return this._locations
+    }
+
+
 }
--- src/backend/routes/routes.js
+++ src/backend/routes/routes.js
@@ -3,15 +3,42 @@ import User from 'app/models/user'
 
 let router = express.Router();
 
-router.route('/users/:username')
+//Integration Test: when username not provided
+//Integration Test: when content-type is not application/json
+router.route('/users')
+    .post((req, res, next) => {
+        //TODO: Controller for handling username input sanitizing and field existance
+        if (req.body.hasOwnProperty('username'))
+            new User(req.body.username).save()
+                .then(username => res.send(username))
+                .catch(err => next(err))
+        else
+            res.status(400).send('"username" property missing from request body.')
+    })   
+
+//User "Posts" a new login attempt
+router.route('/login')
+    .post((req, res, next) => {
+        //TODO: forward error to generic error handler, only set status here, and only set to 401 if username wasn't found
+        if (req.body.hasOwnProperty('username'))
+            new User(req.body.username).get()
+                .then(username => res.send(username))
+                .catch(err => res.status(401).send({"error": err.message}))
+        else
+            res.status(400).send('"username" property missing from request body.')
+    }) 
+
+//TODO: replace with user gotten from session
+router.route('/locations')
+    .post((req, res, next) => {
+        new User(req.body.username).add_location(req.body.location)
+            .then(locations => res.send(locations))
+            .catch(err => next(err))
+    })
     .get((req, res, next) => {
-        try {
-            //TODO: Some kind of sanitation of username, either in model or separate controller
-            res.send(new User(req.params.username).get())
-        }
-        catch (err) {
-            next(err)
-        }
+        new User(req.body.username).get_locations()
+            .then(locations => res.send(locations))
+            .catch(err => next(err))
     })
 
 router.use((req, res, next) => {
