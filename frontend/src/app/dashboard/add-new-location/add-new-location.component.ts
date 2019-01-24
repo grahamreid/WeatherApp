8360ddc49253a44f9b0628dfa4f03a9fbec7ab15
grahamreid <gjreid90@gmail.com>
2019-01-23 20:23:25 -0500

    make new location auto-focus input field

--- frontend/src/app/dashboard/add-new-location/add-new-location.component.ts
+++ frontend/src/app/dashboard/add-new-location/add-new-location.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, TemplateRef, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, TemplateRef, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { LocationService_Rest } from '@app-root/core/http/rest-api.services'
@@ -14,11 +14,14 @@ export class AddNewLocationComponent implements OnInit {
 
   // update dashboard after new location is added
   @Output() refresh: EventEmitter<any> = new EventEmitter();
+
+  @ViewChild("new_location")
+
   // alert if adding location fails (displayed in modal)
   _error = false
 
   modalRef: BsModalRef;
-  constructor(private modalService: BsModalService,
+  constructor(private modal_service: BsModalService,
               private location_service: LocationService_Rest) {
   }
 
@@ -42,7 +45,9 @@ export class AddNewLocationComponent implements OnInit {
         })
   }
 
+  //hacky, really the modal should be its own component but this is a quickfix
   openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template);
-  }
+    this.modalRef = this.modal_service.show(template);
+    setTimeout(() => document.getElementById('new_location').focus(), 100
+    )}
 }
